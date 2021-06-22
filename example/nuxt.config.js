1c2ce823d723cbe4121cf8a1fa4ddef7b314a8e2
Thomas Massmann <thomas.massmann@it-spir.it>
2021-06-22 12:05:49 +0200

    feat: add static target for example site and generate command to trigger static build

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -4,6 +4,10 @@ module.exports = {
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
+  generate: {
+    dir: resolve(__dirname, 'dist')
+  },
+  target: 'static',
   modules: [{ handler: require('../src/module') }],
   buildModules: ['../src/module.js']
 }
---
+++ example/pages/_.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ item.title }}</h1>
+    <h2>{{ item.lastmod }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ params, error, payload }) {
+    if (payload) {
+      return { item: payload }
+    } else {
+      return { item: { title: 'Unknown', lastmod: 'now' } }
+    }
+  }
+}
+</script>
+
+<style></style>
