2d32cb185d5b12fbfe18e95a94d25be38499e826
Thomas Massmann <thomas.massmann@it-spir.it>
2021-10-19 09:02:50 -0400

    refactor(plugin): :recycle: use es6 syntax

--- templates/plugins/plone.js
+++ templates/plugins/plone.js
@@ -274,7 +274,7 @@ class PloneAPI extends Hookable {
           // Check if image source is not relative
           // (does not contain '../../')
           if (imgSrc.indexOf('../') !== -1) {
-            // Reqrite img src to absolute URL
+            // Rewrite img src to absolute URL
             imgSrc = absoluteURL(content['@id'], imgSrc)
           }
 
@@ -302,14 +302,14 @@ class PloneAPI extends Hookable {
 }
 
 function absoluteURL(base, relative) {
-  var stack = base.split("/"),
-    parts = relative.split("/");
+  const stack = base.split("/")
+  const parts = relative.split("/");
   stack.pop(); // remove current file name (or empty string)
   // (omit if "base" is the current folder without trailing slash)
-  for (var i = 0; i < parts.length; i++) {
-    if (parts[i] == ".")
+  for (let i = 0; i < parts.length; i++) {
+    if (parts[i] === ".")
       continue;
-    if (parts[i] == "..")
+    if (parts[i] === "..")
       stack.pop();
     else
       stack.push(parts[i]);
